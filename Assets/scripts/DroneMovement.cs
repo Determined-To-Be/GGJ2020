df8216168f5524862a1e40154bfd6a4c9d3d79cb
Carter Andrews <andrewscart@gmail.com>
2020-01-31 23:37:29 -0800

    tank control for drone movement

---
+++ Assets/scripts/DroneMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    public float throttle;
+    public Vector2 rotationInput;
+    Rigidbody2D rb;
+    public bool testMovement;
+    [SerializeField]
+    float maxSpeed;
+    [SerializeField]
+    float maxAcceleration;
+    [SerializeField]
+    float maxAngularSpeed;
+    [SerializeField]
+    float maxAngularAcceleration;
+    [SerializeField]
+    float drag;
+    Vector2 velocity;
+    Vector2 angularVelocity;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+
+
+        if (testMovement)
+            TestDroneInput();
+        
+        Vector3 desiredVelocity = throttle*transform.up * maxSpeed;
+
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x =
+            Mathf.MoveTowards(rb.velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.y =
+            Mathf.MoveTowards(rb.velocity.y, desiredVelocity.y, maxSpeedChange);
+        if (Vector3.zero == desiredVelocity)
+            velocity *= drag;
+        rb.velocity = velocity;
+        angularVelocity= Vector3.RotateTowards(angularVelocity, rotationInput, maxAngularAcceleration, 1);
+        transform.up = angularVelocity;
+    }
+    void TestDroneInput()
+    {
+        throttle =  Input.GetAxis("Vertical");
+        rotationInput = Vector3.RotateTowards(transform.up, Input.GetAxis("Horizontal")*transform.right, 0.1f, 0);
+    }
+    public void SetRotationInput(Vector2 newRot)
+    {
+        rotationInput = newRot;
+    }
+}
