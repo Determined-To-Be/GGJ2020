b6ad2c5fe4a0d22fb391ee6d3197c0e41806434e
Jacob <jacob@goldenneedham.com>
2020-02-01 10:15:45 -0800

    Raycasts

---
+++ Assets/scripts/Panel Scritps/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = this.gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            if(Physics.Raycast(this.transform.position, cam.ScreenToWorldPoint(Input.mousePosition), out hit)){
+                //DO SHIT
+            }
+            
+        }
+    }
+}
