2af297ea7063c786ea44a35d2bbd22f23c1ee7ef
Jacob <jacob@goldenneedham.com>
2020-02-01 13:15:32 -0800

    Moved to HDRP

---
+++ Assets/scripts/Panel Scritps/Dial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial : PanelObject
+{
+    float currAngle = 0;
+    public int ticks = 16;
+    public bool useLimits = false;
+    public float minAngle = 0, maxAngle = 0;
+
+    public override void OnHold(){
+        //Store the initial position of the mouse
+        //Calculate the difference between the current and new position
+        //Using the difference calculate the angle to add to the dial
+        //Rince and repeat
+    }
+
+    public override void OnDown(){
+        base.OnDown();
+    }
+
+    public override void OnUp(){
+        base.OnUp();
+    }
+
+}
