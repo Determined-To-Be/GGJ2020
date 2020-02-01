ee378df463b2b28d2a070c33849d373aec91c91c
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-02-01 13:19:15 -0800

    deleted packages

--- Assets/scripts/PirateController.cs
+++ Assets/scripts/PirateController.cs
@@ -5,30 +5,41 @@ using UnityEngine.AI;
 
 public class PirateController : MonoBehaviour
 { 
-    public float speed;
-    public NavMeshAgent nav;
-    public Transform playerPos;
-    public bool aggro = true;
-    public Transform[] waypoints;
+    //public float speed;
+    //public NavMeshAgent nav;
+    //public Transform playerPos;
+    //public bool aggro = true;
+    //public Transform[] waypoints;
+    //public int numberOfWaypoints = 0;
 
-    private void Start()
-    {
-        nav.speed = speed;
-        
-    }
+    //private void Start()
+    //{
+    //    nav.speed = speed;
+    //    // setting speed of enemy
+    //    GotoNextPoint();
+    //}
+    //void GotoNextPoint()
+    //{
+    //    if (waypoints.Length == 0)
+    //        return;
+    //    nav.destination = waypoints[numberOfWaypoints].position;
+    //    numberOfWaypoints = (numberOfWaypoints + 1) % waypoints.Length;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (aggro)
-        {
-            nav.destination = playerPos.position;
-            // if enemy knows where player already is
-        }
-        else
-        {
-            nav.destination = waypoints[0].position;
-        }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (!nav.pathPending && nav.remainingDistance < 0.5f)
+    //    {
+    //        GotoNextPoint();
+    //    }
+    //    else
+    //    {
+    //        nav.destination = playerPos.position;
+    //        // if enemy knows where player already is
+    //        // enemy will go to player's position
+    //    }
 
-    }
+    //}
+    
 }
