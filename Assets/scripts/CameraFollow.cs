7d00c3589fdb74f7704fd3c2ff068243d1198321
Carter Andrews <andrewscart@gmail.com>
2020-02-01 11:23:56 -0800

    visual upgrade for drone movement

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,target.position + offset,smoothing);
+    }
+}
