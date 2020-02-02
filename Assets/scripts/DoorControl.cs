d49ffed0dd45126562bce99648a1629a21a77da7
Carter Andrews <andrewscart@gmail.com>
2020-02-01 16:47:11 -0800

    doooooooooooooooorrrrrrrrrrrrrrrrrr

--- Assets/scripts/DoorControl.cs
+++ Assets/scripts/DoorControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DoorControl : MonoBehaviour
 {
+
+    Animator anim;
+    public bool testing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O)&&testing)
+        {
+            toggleDoor();
+        }
+    }
+    public void toggleDoor()
+    {
+        anim.SetBool("closed", !anim.GetBool("closed"));
     }
 }
