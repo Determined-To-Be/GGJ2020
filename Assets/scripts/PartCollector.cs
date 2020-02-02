64753ec002e7540dddc603c23f957bc8e564db6f
Carter Andrews <andrewscart@gmail.com>
2020-02-01 19:13:37 -0800

    home and part prefabs

---
+++ Assets/scripts/PartCollector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartCollector : MonoBehaviour
+{
+    public bool partsCollected;
+    public GameObject partSprite;
+      // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.collider.name);
+        if ((!partsCollected) && collision.collider.CompareTag("part"))
+        {
+            partSprite.active = true;
+            Destroy(collision.collider.gameObject);
+            partsCollected = true;
+        }
+        if (collision.collider.CompareTag("home"))
+        {
+            partSprite.active = false;
+            partsCollected = false;
+            //transition to n
+        }
+    }
+        private void OnTriggerEnter(Collider other)
+    {
+        if (!partsCollected&&other.CompareTag("part"))
+        {
+            partSprite.active = true;
+            Destroy(other.gameObject);
+            partsCollected = true;
+        }
+        if (other.CompareTag("home"))
+        {
+            partSprite.active = false;
+            partsCollected = false;
+            //transition to next level
+        }
+    }
+}
