12432d6f43f74e1abbba96517bc7bf666dfb1f28
Jacob <jacob@goldenneedham.com>
2020-02-01 23:11:16 -0800

    Working buttons and switches

--- Assets/scripts/Panel Scritps/Button.cs
+++ Assets/scripts/Panel Scritps/Button.cs
@@ -7,8 +7,11 @@ public class Button : PanelObject
 {
     
     private float pitchMod;
+    public float pushDepth = -1;
+    public Vector3 initPos;
 
     public void Start(){
+        initPos = this.transform.localPosition;
         pitchMod = Random.Range(.5f, 1);
     }
 
@@ -22,12 +25,14 @@ public class Button : PanelObject
     }
 
     public override void OnDown(){
+        this.transform.localPosition = initPos + new Vector3(0, pushDepth, 0);
         AudioManager.Instance.PlaySoundOnce(AudioManager.Channel.player, AudioManager.Instance.GetSample("player_button_push"), 1, 1 * pitchMod);
         onDown.Invoke();
     }
 
     public override void OnUp(){
+        this.transform.localPosition = initPos;
         AudioManager.Instance.PlaySoundOnce(AudioManager.Channel.player, AudioManager.Instance.GetSample("player_button_release"), 1, 1 * pitchMod);
-        onDown.Invoke();
+        onUp.Invoke();
     }
 }
--- Assets/scripts/Panel Scritps/ToggleButton.cs
+++ Assets/scripts/Panel Scritps/ToggleButton.cs
@@ -8,11 +8,14 @@ public class ToggleButton : PanelObject
 
     private float pitchMod;
 
+    public Vector3 rotationVector;
+    public Vector2 rotationLimits;
+
     [SerializeField]
     UnityEvent  onToggleDown = new UnityEvent(),
                 onToggleUp = new UnityEvent();
 
-    bool state = false;
+    public bool state = false;
 
     public void Start(){
         pitchMod = Random.Range(.5f, 1);
@@ -23,12 +26,16 @@ public class ToggleButton : PanelObject
 
     public override void OnDown(){
         state = !state;
-        AudioManager.Instance.PlaySoundOnce(AudioManager.Channel.player, AudioManager.Instance.GetSample("player_button_push"), 1, 1 * pitchMod);
         if(state){
+            print(new Vector3(rotationVector.x * rotationLimits.x, rotationVector.y * rotationLimits.x, rotationVector.z * rotationLimits.x));
+            this.transform.localRotation = Quaternion.Euler(rotationVector.x * rotationLimits.x, rotationVector.y * rotationLimits.x, rotationVector.z * rotationLimits.x);
             onToggleUp.Invoke();
         } else {
+            print(new Vector3(rotationVector.x * rotationLimits.y, rotationVector.y * rotationLimits.y, rotationVector.z * rotationLimits.y));
+            this.transform.localRotation = Quaternion.Euler(rotationVector.x * rotationLimits.y, rotationVector.y * rotationLimits.y, rotationVector.z * rotationLimits.y);
             onToggleDown.Invoke();
         }
+        AudioManager.Instance.PlaySoundOnce(AudioManager.Channel.player, AudioManager.Instance.GetSample("player_button_push"), 1, 1 * pitchMod);
     }
 
     public override void OnUp(){
