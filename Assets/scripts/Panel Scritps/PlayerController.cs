93bde423269486ed120f43c4686746ac49d498a9
Kain Shepard <kainkun100@gmail.com>
2020-02-02 16:59:32 -0800

    mat plugged in

--- Assets/scripts/Panel Scritps/PlayerController.cs
+++ Assets/scripts/Panel Scritps/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 
     Camera cam;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         cam = this.gameObject.GetComponent<Camera>();
         this.transform.tag = "MainCamera";
