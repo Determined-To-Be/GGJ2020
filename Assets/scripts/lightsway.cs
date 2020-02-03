6aad0e7a3ee0a3c9fa9a26fff69c0bee310dbcf9
Kain Shepard <kainkun100@gmail.com>
2020-02-02 16:40:04 -0800

    light test

---
+++ Assets/scripts/lightsway.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightsway : MonoBehaviour
+{
+    public float amp;
+    public float speed;
+
+    public float Hamp;
+    Vector3 start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = start + new Vector3(Mathf.Sin(Time.time * speed) * amp, Mathf.Cos(Time.time*speed*2)*Hamp, 0);
+    }
+}
