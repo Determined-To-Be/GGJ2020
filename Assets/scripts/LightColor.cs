7db7c32135df1c8327f3d1da36821381db463b6f
Jacob <jacob@goldenneedham.com>
2020-02-02 16:26:54 -0800

    light get auto

--- Assets/scripts/LightColor.cs
+++ Assets/scripts/LightColor.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class LightColor : MonoBehaviour
 {
 
     public Color color;
     Material mat;
-    public Light light;
+    Light light;
     // Start is called before the first frame update
     void Start()
     {
+        light = this.GetComponentInChildren<Light>();
         mat = this.GetComponent<Material>();
         light.color = color;
         mat.SetColor("_EmissionColor", color); 
