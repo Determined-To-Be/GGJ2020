2578edfa77d1582fd06a7c3a3399415ddf6474a0
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-02-03 21:20:14 -0800

    quit switch and flash lights

--- Assets/scripts/LightColor.cs
+++ Assets/scripts/LightColor.cs
@@ -8,6 +8,9 @@ public class LightColor : MonoBehaviour
     public Color color;
     Material mat;
     Light light;
+    Coroutine flash;
+
+    float speed = 1f;
 
     public Color active, inactive;
     // Start is called before the first frame update
@@ -41,6 +44,40 @@ public class LightColor : MonoBehaviour
         this.color = active;
     }
 
+    public void FlashStart()
+    {
+        flash = StartCoroutine(Flashing());
+    }
+
+    public void FlashStop()
+    {
+        deactivate();
+        StopCoroutine(flash);
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            float ratio = 0f;
+            while (this.color != active)
+            {
+                ratio += Time.deltaTime * speed;
+                if (ratio > 1f) ratio = 1f;
+                this.color = Color.Lerp(inactive, active, ratio);
+                yield return null;
+            }
+            ratio = 0f;
+            while (this.color != inactive)
+            {
+                ratio += Time.deltaTime * speed;
+                if (ratio > 1f) ratio = 1f;
+                this.color = Color.Lerp(active, inactive, ratio);
+                yield return null;
+            }
+            yield return null;
+        }
+    }
 
     public void deactivate(){
         this.color = inactive;
---
+++ Assets/scripts/Quit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    Coroutine noWaitDont;
+
+    public void EndTheSimulation()
+    {
+        noWaitDont = StartCoroutine(Stop());
+    }
+
+    public void Cancel()
+    {
+        StopCoroutine(noWaitDont);
+    }
+
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(5f);
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+        Application.OpenURL("https://sbarrack.itch.io/drone-seamulator");
+        #else
+        Application.Quit();
+        #endif
+        yield return null;
+    }
+}
